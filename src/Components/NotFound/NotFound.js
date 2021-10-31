e2dfd389c62512d61ae2c4a6507476bad3f84cc0
Md. Ariful Islam <arifultusharr@gmail.com>
2021-10-31 17:04:40 +0600

    not found added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,12 +1,12 @@
 import React from "react";
+import notfound from "../../images/404.png";
 
 const NotFound = () => {
   return (
     <div>
-      <h2>This is not found page</h2>
+      <img className="w-2/3 lg:w-1/2 mx-auto" src={notfound} alt="" />
     </div>
   );
 };
 
 export default NotFound;
-<h2>This is not found page</h2>;
